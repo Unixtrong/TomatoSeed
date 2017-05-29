d8839d6170bc7e89e9a445072762968d4b7c2446
Aloong <innocentcheng@163.com>
2017-05-29 20:41:52 +0800

    新建三个包

---
+++ app/src/main/java/com/huangshan/tomatoseed/result/ui/ResultActivity.java
@@ -0,0 +1,15 @@
+package com.huangshan.tomatoseed.result.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.huangshan.tomatoseed.R;
+
+public class ResultActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+    }
+}
